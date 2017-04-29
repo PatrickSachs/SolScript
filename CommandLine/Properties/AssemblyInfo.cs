7828b34e92cda89acd7602c81ccfe5cf18c566a7
Patrick Sachs <patrick.sachs@ymail.com>
2017-04-29 03:19:40 +0200

    Changed project/file structure. - SolScript_Library - Changed to SolScript - SolScript - Changed to CommandLine - Added UnitTests

--- CommandLine/Properties/AssemblyInfo.cs
+++ CommandLine/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("SolScript_Library")]
-[assembly: AssemblyDescription("A truly stellar scripting language.")]
+[assembly: AssemblyTitle("SolScript")]
+[assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Patrick Sachs")]
+[assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("SolScript")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("fa9b0a5f-98c6-48d6-a8c3-b47336b60990")]
+[assembly: Guid("48eebfb7-73d6-407f-b4cb-9df79ce00ea0")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.3.1.*")]
-[assembly: AssemblyFileVersion("0.3")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
--- UnitTests/Properties/AssemblyInfo.cs
+++ UnitTests/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("SolScript_Library")]
-[assembly: AssemblyDescription("A truly stellar scripting language.")]
+[assembly: AssemblyTitle("UnitTests")]
+[assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Patrick Sachs")]
-[assembly: AssemblyProduct("SolScript")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTests")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("fa9b0a5f-98c6-48d6-a8c3-b47336b60990")]
+[assembly: Guid("6389b046-cc6e-445b-9024-8ebde3e6e595")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.3.1.*")]
-[assembly: AssemblyFileVersion("0.3")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
